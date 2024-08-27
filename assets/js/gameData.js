d0624c63a9c1b124f418d315dfce94be4dea5741
Anne Voretzsch <ac.voretzsch@gmx.de>
2024-08-27 20:56:27 +0000

    Add addtional questions and data to gameData.js.

--- assets/js/gameData.js
+++ assets/js/gameData.js
@@ -1,7 +1,73 @@
-// Questions for Verb category
-let verbsQuestions = [{
-    // Tier One Question
-    question: "Find the the synonym for understand",
-    answers: ["comprehend", "master", "think", "imagine"],
-    correctAnswer: 0
-}]
+// Questions for verbs category
+let verbsQuestions = [
+
+    // Tier One question
+    {
+        question: "Find the the synonym for understand",
+        answers: ["comprehend", "master", "think", "imagine"],
+        correctAnswer: 0
+    },
+
+    // Tier Two question
+    {
+        question: "Find the synonym for imply",
+        answers: ["mislead", "narrate", "indicate", "affirm"],
+        correctAnswer: 2
+    },
+
+    // Tier Three question
+    {
+        question: "Find the synonym for argue",
+        answers: ["prevaricate", "quibble", "equivocate", "agitate"],
+        correctAnswer: 1
+    },
+
+]
+
+let nounsQuestions = [
+
+    // Tier One question
+    {
+        question: "Find the synonym for decision",
+        answers: ["cause", "timidity", "resolution", "evaluation"],
+        correctAnswer: 2
+    },
+
+    // Tier Two question
+    {
+        question: "Find the synonym for competition",
+        answers: ["tranquility", "appeal", "dispute", "contest"],
+        correctAnswer: 3
+    },
+
+    // Tier Three question
+    {
+        question: "Find the synonym for courage",
+        answers: ["tenacity", "enervation", "apathy", "fortitude"],
+        correctAnswer: 0
+    },
+]
+
+let adjectivesQuestions = [
+
+    // Tier One question
+    {
+        question: "Find the synonym for observant",
+        answers: ["indifferent", "savvy", "perceptive", "logical"],
+        correctAnswer: 3
+    },
+
+    // Tier Two question
+    {
+        question: "Find the synonym for jealous",
+        answers: ["suspicious", "envious", "dissatisfied", "fastidious"],
+        correctAnswer: 1
+    },
+
+    // Tier Three question
+    {
+        question: "Find the synonym for cautious",
+        answers: ["judicious", "nifty", "invidious", "nonsensical"],
+        correctAnswer: 0
+    },
+]
