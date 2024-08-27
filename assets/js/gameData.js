41eeca9af76c7a00e048ad1d6ddf44885e4f40a9
Anne Voretzsch <ac.voretzsch@gmx.de>
2024-08-27 12:36:59 +0000

    Add ne gameData.js file to contain all game questions and answers to make both js files easier to read.

---
+++ assets/js/gameData.js
@@ -0,0 +1,12 @@
+let questionsVerbs = [
+
+    { 
+        question:"Find the the synonym for understand",
+        answers:[
+            {text:"comprehend", correct: true},
+            {text:"master", correct: false},
+            {text:"think", correct: false},
+            {text:"imagine", correct: false},
+        ]
+    }
+]
